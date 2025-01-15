b8084b21971e873ed67d7a1590578f376d7e49f1
Haniel Ubogu <hanieltobi@gmail.com>
2025-01-15 21:21:14 +0000

    feat(site): update layout metadata for improved SEO

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,13 @@
 </script>
 
 <svelte:head>
-	<title>Svelte DnD Kit</title>
+	<title>svelte-dnd-kit - a modern drag and drop toolkit for svelte</title>
+	<meta
+		name="description"
+		content="svelte-dnd-kit is a modern drag and drop toolkit for svelte based on dnd-kit for react"
+	/>
+	<meta name="keywords" content="svelte, dnd, dnd-kit, drag and drop, toolkit" />
+	<meta name="author" content="HanielU" />
 </svelte:head>
 
 {@render children()}
